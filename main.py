212f64ad457fca638914073bb4fc0f56a0327faf
Joshua Daniels-Kemp (kempj8) <kempj8@coventry.ac.uk>
2018-12-01 12:46:12 +0000

    Almost something

---
+++ main.py
@@ -0,0 +1,8 @@
+import dns.resolver
+
+my_resolver = dns.resolver.Resolver()
+
+# 8.8.8.8 is Google's public DNS server
+my_resolver.nameservers = ['8.8.8.8']
+
+answer = my_resolver.query('google.com')
